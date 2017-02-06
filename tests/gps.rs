70356dba1155dd5995729276f57d1926ea901e13
Samuel Tardieu <sam@rfc1149.net>
2017-02-06 11:37:48 +0100

    Simpler neighbours indication

--- tests/gps.rs
+++ tests/gps.rs
@@ -26,35 +26,35 @@ impl Coords {
 }
 
 fn coords() -> HashMap<&'static str, Coords> {
-    let mut locations = HashMap::new();
-    locations.insert("Paris", Coords(48.8567, 2.3508));
-    locations.insert("Lyon", Coords(45.76, 4.84));
-    locations.insert("Marseille", Coords(43.2964, 5.37));
-    locations.insert("Bordeaux", Coords(44.84, -0.58));
-    locations.insert("Cannes", Coords(43.5513, 7.0128));
-    locations.insert("Toulouse", Coords(43.6045, 1.444));
-    locations.insert("Reims", Coords(49.2628, 4.0347));
-    locations
+    vec![("Paris", Coords(48.8567, 2.3508)),
+         ("Lyon", Coords(45.76, 4.84)),
+         ("Marseille", Coords(43.2964, 5.37)),
+         ("Bordeaux", Coords(44.84, -0.58)),
+         ("Cannes", Coords(43.5513, 7.0128)),
+         ("Toulouse", Coords(43.6045, 1.444)),
+         ("Reims", Coords(49.2628, 4.0347))]
+        .into_iter()
+        .collect()
 }
 
 fn neighbour_distances(coords: &HashMap<&str, Coords>)
                        -> HashMap<&'static str, Vec<(&'static str, u64)>> {
     let mut neighbours = HashMap::new();
     {
-        let mut insert_neighbour = |from: &'static str, to: &[&'static str]| {
+        let mut insert_neighbour = |from: &'static str, to: &'static str| {
             let from_coords = &coords[from];
-            let ns = to.into_iter()
-                .map(|&neighbour| (neighbour, from_coords.distance_in_meters(&coords[neighbour])))
+            let ns = to.split(',')
+                .map(|neighbour| (neighbour, from_coords.distance_in_meters(&coords[neighbour])))
                 .collect();
             neighbours.insert(from, ns);
         };
-        insert_neighbour("Paris", &["Lyon", "Bordeaux", "Reims"]);
-        insert_neighbour("Lyon", &["Paris", "Marseille"]);
-        insert_neighbour("Marseille", &["Lyon", "Cannes", "Toulouse"]);
-        insert_neighbour("Bordeaux", &["Toulouse", "Paris"]);
-        insert_neighbour("Cannes", &["Marseille"]);
-        insert_neighbour("Toulouse", &["Marseille", "Bordeaux"]);
-        insert_neighbour("Reims", &["Paris"]);
+        insert_neighbour("Paris", "Lyon,Bordeaux,Reims");
+        insert_neighbour("Lyon", "Paris,Marseille");
+        insert_neighbour("Marseille", "Lyon,Cannes,Toulouse");
+        insert_neighbour("Bordeaux", "Toulouse,Paris");
+        insert_neighbour("Cannes", "Marseille");
+        insert_neighbour("Toulouse", "Marseille,Bordeaux");
+        insert_neighbour("Reims", "Paris");
     }
     neighbours
 }
@@ -63,8 +63,7 @@ fn neighbour_distances(coords: &HashMap<&str, Coords>)
 fn test_gps() {
     let coords = coords();
     let neighbour_distances = neighbour_distances(&coords);
-    let start = "Paris";
-    let goal = "Cannes";
+    let (start, goal) = ("Paris", "Cannes");
     let goal_coords = &coords[goal];
     let expected_path = vec!["Paris", "Lyon", "Marseille", "Cannes"];
 
