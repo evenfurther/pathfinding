c5db94b1c238847a1e2bf8de43a04f95d0b3f485
Samuel Tardieu <sam@rfc1149.net>
2017-11-02 13:46:54 +0100

    Use a manipulable structure for EdmondsKarp

--- src/edmonds_karp.rs
+++ src/edmonds_karp.rs
@@ -76,68 +76,102 @@ pub fn edmonds_karp_matrix<C>(
 where
     C: Zero + Signed + Bounded + PartialOrd + Copy,
 {
-    let size = capacities.shape()[0];
-    assert_eq!(capacities.shape()[0], capacities.shape()[1]);
-    if source >= size || sink >= size {
-        return (vec![], Zero::zero());
-    }
-    // Permanent evoluting data structures.
-    let mut flows = Array2::<C>::zeros((size, size));
-    let mut total_capacity = Zero::zero();
-    let mut to_see = VecDeque::new();
+    let mut ek = EdmondsKarp::new(source, sink, capacities);
+    ek.augment()
+}
 
-    // Data which will be cleared for every path but
-    // is allocated once.
-    let mut parents = Array1::from_elem(size, None);
-    let mut path_capacity = Array1::from_elem(size, C::max_value());
+/// Structure holding Edmonds-Karp algorithm internal variables. Only
+/// `capacities` may be manipulated from outside in order to gradually
+/// change the capacities.
+pub struct EdmondsKarp<'a, C: 'a> {
+    source: usize,
+    sink: usize,
+    size: usize,
+    capacities: &'a Array2<C>,
+    flows: Array2<C>,
+    total_capacity: C,
+    parents: Array1<Option<usize>>,
+    path_capacity: Array1<C>,
+}
 
-    // Repeatidly look for an augmenting path.
-    'augment: loop {
-        to_see.clear();
-        to_see.push_back(source);
-        while let Some(node) = to_see.pop_front() {
-            let capacity_so_far = path_capacity[node];
-            for neighbour in 0..size {
-                let residual = capacities[[node, neighbour]] - flows[[node, neighbour]];
-                if neighbour == source || residual <= Zero::zero() || parents[neighbour].is_some() {
-                    continue;
-                }
-                parents[neighbour] = Some(node);
-                path_capacity[neighbour] = if capacity_so_far < residual {
-                    capacity_so_far
-                } else {
-                    residual
-                };
-                if neighbour == sink {
-                    let mut n = sink;
-                    while n != source {
-                        let p = parents[n].unwrap();
-                        flows[[p, n]] = flows[[p, n]] + path_capacity[sink];
-                        flows[[n, p]] = flows[[n, p]] - path_capacity[sink];
-                        n = p;
-                    }
-                    total_capacity = total_capacity + path_capacity[sink];
-                    parents.fill(None);
-                    path_capacity.fill(C::max_value());
-                    continue 'augment;
-                }
-                to_see.push_back(neighbour);
-            }
+impl<'a, C> EdmondsKarp<'a, C>
+where
+    C: 'a + Zero + Signed + Bounded + PartialOrd + Copy,
+{
+    /// Create a new `EdmondsKarp` structure.
+    pub fn new(source: usize, sink: usize, capacities: &'a Array2<C>) -> EdmondsKarp<'a, C> {
+        let size = capacities.shape()[0];
+        assert_eq!(capacities.shape()[0], capacities.shape()[1]);
+        EdmondsKarp {
+            source: source,
+            sink: sink,
+            size: size,
+            capacities: capacities,
+            flows: Array2::<C>::zeros((size, size)),
+            total_capacity: Zero::zero(),
+            parents: Array1::from_elem(size, None),
+            path_capacity: Array1::from_elem(size, C::max_value()),
         }
-        break;
     }
-    (
-        indices_of(&flows)
-            .into_iter()
-            .filter_map(|(a, b)| {
-                let f = flows[[a, b]];
-                if f > Zero::zero() {
-                    Some(((a, b), f))
-                } else {
-                    None
+
+    /// Augment paths so that the flow is maximal.
+    pub fn augment(&mut self) -> EKFlows<usize, C> {
+        if self.source >= self.size || self.sink >= self.size {
+            return (vec![], Zero::zero());
+        }
+        let mut to_see = VecDeque::new();
+        'augment: loop {
+            to_see.clear();
+            to_see.push_back(self.source);
+            while let Some(node) = to_see.pop_front() {
+                let capacity_so_far = self.path_capacity[node];
+                for neighbour in 0..self.size {
+                    let residual =
+                        self.capacities[[node, neighbour]] - self.flows[[node, neighbour]];
+                    if neighbour == self.source || residual <= Zero::zero()
+                        || self.parents[neighbour].is_some()
+                    {
+                        continue;
+                    }
+                    self.parents[neighbour] = Some(node);
+                    self.path_capacity[neighbour] = if capacity_so_far < residual {
+                        capacity_so_far
+                    } else {
+                        residual
+                    };
+                    if neighbour == self.sink {
+                        let mut n = self.sink;
+                        while n != self.source {
+                            let p = self.parents[n].unwrap();
+                            self.flows[[p, n]] = self.flows[[p, n]] + self.path_capacity[self.sink];
+                            self.flows[[n, p]] = self.flows[[n, p]] - self.path_capacity[self.sink];
+                            n = p;
+                        }
+                        self.total_capacity = self.total_capacity + self.path_capacity[self.sink];
+                        self.parents.fill(None);
+                        self.path_capacity.fill(C::max_value());
+                        continue 'augment;
+                    }
+                    to_see.push_back(neighbour);
                 }
-            })
-            .collect(),
-        total_capacity,
-    )
+            }
+            self.parents.fill(None);
+            self.path_capacity.fill(C::max_value());
+            break;
+        }
+        (
+            indices_of(&self.flows)
+                .into_iter()
+                .filter_map(|(a, b)| {
+                    let f = self.flows[[a, b]];
+                    if f > Zero::zero() {
+                        Some(((a, b), f))
+                    } else {
+                        None
+                    }
+                })
+                .collect(),
+            self.total_capacity,
+        )
+    }
 }
