2306109268536d60542d19145c3539e656551db7
Samuel Tardieu <sam@rfc1149.net>
2017-11-13 15:39:57 +0100

    Factorize examples

--- tests/edmondskarp.rs
+++ tests/edmondskarp.rs
@@ -39,146 +39,110 @@ fn check_wikipedia_result(flows: EKFlows<char, i32>) {
     assert_eq!(total, 5);
 }
 
-#[test]
-fn wikipedia_example_dense() {
-    check_wikipedia_result(edmonds_karp_dense(
+fn wikipedia_example<EK: EdmondsKarp<i32>>() {
+    check_wikipedia_result(edmonds_karp::<_, _, _, EK>(
         &"ABCDEFGH".chars().collect::<Vec<_>>(),
         &'A',
         &'G',
         neighbours_wikipedia(),
     ));
+}
+
+#[test]
+fn wikipedia_example_dense() {
+    wikipedia_example::<DenseCapacity<_>>();
 }
 
 #[test]
 fn wikipedia_example_sparse() {
-    check_wikipedia_result(edmonds_karp_sparse(
-        &"ABCDEFGH".chars().collect::<Vec<_>>(),
-        &'A',
-        &'G',
-        neighbours_wikipedia(),
-    ));
+    wikipedia_example::<SparseCapacity<_>>();
+}
+
+fn wikipedia_progressive_example<EK: EdmondsKarp<i32>>() {
+    let neighbours = neighbours_wikipedia();
+    let size = neighbours.len();
+    let mut ek = EK::new(size, 0, 6);
+    for ((from, to), cap) in neighbours {
+        let (_, total) = ek.augment();
+        assert!(total < 5);
+        ek.set_capacity(from as usize - 65, to as usize - 65, cap);
+    }
+    let (caps, total) = ek.augment();
+    let caps = caps.into_iter()
+        .map(|((from, to), cap)| {
+            (((from + 65) as u8 as char, (to + 65) as u8 as char), cap)
+        })
+        .collect::<Vec<_>>();
+    check_wikipedia_result((caps, total));
 }
 
 #[test]
 fn wikipedia_progressive_example_dense() {
-    let neighbours = neighbours_wikipedia();
-    let size = neighbours.len();
-    let mut ek = DenseCapacity::new(size, 0, 6);
-    for ((from, to), cap) in neighbours {
-        let (_, total) = ek.augment();
-        assert!(total < 5);
-        ek.set_capacity(from as usize - 65, to as usize - 65, cap);
-    }
-    let (caps, total) = ek.augment();
-    let caps = caps.into_iter()
-        .map(|((from, to), cap)| {
-            (((from + 65) as u8 as char, (to + 65) as u8 as char), cap)
-        })
-        .collect::<Vec<_>>();
-    check_wikipedia_result((caps, total));
+    wikipedia_progressive_example::<DenseCapacity<_>>();
 }
 
 #[test]
 fn wikipedia_progressive_example_sparse() {
-    let neighbours = neighbours_wikipedia();
-    let size = neighbours.len();
-    let mut ek = SparseCapacity::new(size, 0, 6);
-    for ((from, to), cap) in neighbours {
-        let (_, total) = ek.augment();
-        assert!(total < 5);
-        ek.set_capacity(from as usize - 65, to as usize - 65, cap);
-    }
-    let (caps, total) = ek.augment();
-    let caps = caps.into_iter()
-        .map(|((from, to), cap)| {
-            (((from + 65) as u8 as char, (to + 65) as u8 as char), cap)
-        })
-        .collect::<Vec<_>>();
-    check_wikipedia_result((caps, total));
+    wikipedia_progressive_example::<SparseCapacity<_>>();
+}
+
+fn disconnected<EK: EdmondsKarp<isize>>() {
+    let (caps, total) = edmonds_karp::<_, _, _, EK>(
+        &['A', 'B'],
+        &'A',
+        &'B',
+        std::iter::empty::<((char, char), isize)>(),
+    );
+    assert_eq!(caps.len(), 0);
+    assert_eq!(total, 0);
 }
 
 #[test]
 fn disconnected_dense() {
-    let (caps, total) = edmonds_karp_dense(
-        &['A', 'B'],
-        &'A',
-        &'B',
-        std::iter::empty::<((char, char), isize)>(),
-    );
-    assert_eq!(caps.len(), 0);
-    assert_eq!(total, 0);
+    disconnected::<DenseCapacity<_>>();
 }
 
 #[test]
 fn disconnected_sparse() {
-    let (caps, total) = edmonds_karp_sparse(
-        &['A', 'B'],
-        &'A',
-        &'B',
-        std::iter::empty::<((char, char), isize)>(),
-    );
-    assert_eq!(caps.len(), 0);
-    assert_eq!(total, 0);
+    disconnected::<SparseCapacity<_>>();
+}
+
+fn modified<EK: EdmondsKarp<i32>>() {
+    // Graph is:
+    //
+    // 0 --> 1 --> 2 --> 3
+    // |---> 4 --> 5 ----^
+    //
+    // Upper branch has capacity 5, lower branch 4.
+    let mut ek = EK::new(6, 0, 3);
+    ek.set_capacity(0, 1, 5);
+    ek.set_capacity(1, 2, 5);
+    ek.set_capacity(2, 3, 5);
+    ek.set_capacity(0, 4, 4);
+    ek.set_capacity(4, 5, 4);
+    ek.set_capacity(5, 3, 4);
+    assert_eq!(ek.augment().1, 9);
+    // Set lower branch capacity to 5.
+    ek.set_capacity(0, 4, 5);
+    ek.set_capacity(4, 5, 5);
+    ek.set_capacity(5, 3, 5);
+    assert_eq!(ek.augment().1, 10);
+    // Try setting lower branch individual capacities
+    // to 4 one at a time.
+    for &(from, to) in &[(0, 4), (4, 5), (5, 3)] {
+        ek.set_capacity(from, to, 4);
+        assert_eq!(ek.augment().1, 9);
+        ek.set_capacity(from, to, 5);
+        assert_eq!(ek.augment().1, 10);
+    }
 }
 
 #[test]
 fn modified_dense() {
-    // Graph is:
-    //
-    // 0 --> 1 --> 2 --> 3
-    // |---> 4 --> 5 ----^
-    //
-    // Upper branch has capacity 5, lower branch 4.
-    let mut ek = DenseCapacity::new(6, 0, 3);
-    ek.set_capacity(0, 1, 5);
-    ek.set_capacity(1, 2, 5);
-    ek.set_capacity(2, 3, 5);
-    ek.set_capacity(0, 4, 4);
-    ek.set_capacity(4, 5, 4);
-    ek.set_capacity(5, 3, 4);
-    assert_eq!(ek.augment().1, 9);
-    // Set lower branch capacity to 5.
-    ek.set_capacity(0, 4, 5);
-    ek.set_capacity(4, 5, 5);
-    ek.set_capacity(5, 3, 5);
-    assert_eq!(ek.augment().1, 10);
-    // Try setting lower branch individual capacities
-    // to 4 one at a time.
-    for &(from, to) in &[(0, 4), (4, 5), (5, 3)] {
-        ek.set_capacity(from, to, 4);
-        assert_eq!(ek.augment().1, 9);
-        ek.set_capacity(from, to, 5);
-        assert_eq!(ek.augment().1, 10);
-    }
+    modified::<DenseCapacity<i32>>()
 }
 
 #[test]
 fn modified_sparse() {
-    // Graph is:
-    //
-    // 0 --> 1 --> 2 --> 3
-    // |---> 4 --> 5 ----^
-    //
-    // Upper branch has capacity 5, lower branch 4.
-    let mut ek = SparseCapacity::new(6, 0, 3);
-    ek.set_capacity(0, 1, 5);
-    ek.set_capacity(1, 2, 5);
-    ek.set_capacity(2, 3, 5);
-    ek.set_capacity(0, 4, 4);
-    ek.set_capacity(4, 5, 4);
-    ek.set_capacity(5, 3, 4);
-    assert_eq!(ek.augment().1, 9);
-    // Set lower branch capacity to 5.
-    ek.set_capacity(0, 4, 5);
-    ek.set_capacity(4, 5, 5);
-    ek.set_capacity(5, 3, 5);
-    assert_eq!(ek.augment().1, 10);
-    // Try setting lower branch individual capacities
-    // to 4 one at a time.
-    for &(from, to) in &[(0, 4), (4, 5), (5, 3)] {
-        ek.set_capacity(from, to, 4);
-        assert_eq!(ek.augment().1, 9);
-        ek.set_capacity(from, to, 5);
-        assert_eq!(ek.augment().1, 10);
-    }
+    modified::<SparseCapacity<i32>>()
 }
