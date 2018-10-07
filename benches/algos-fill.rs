b0cad6875276133912ee21783f50906e14c9957b
Samuel Tardieu <sam@rfc1149.net>
2018-10-07 18:54:20 +0200

    Use criterion for benchmarking

--- benches/algos-fill.rs
+++ benches/algos-fill.rs
@@ -1,13 +1,12 @@
-#![feature(test)]
-
 // This version uses a filler in the Pt structure to increase
 // the cost of cloning a node.
 
+#[macro_use]
+extern crate criterion;
 extern crate pathfinding;
-extern crate test;
 
+use criterion::Criterion;
 use pathfinding::prelude::{astar, bfs, dfs, dijkstra, fringe, idastar, iddfs};
-use test::Bencher;
 
 #[derive(Clone, Debug, Eq, Hash, Ord, PartialEq, PartialOrd)]
 struct Pt {
@@ -27,7 +26,7 @@ impl Pt {
 
     #[inline]
     fn heuristic(p: &Pt) -> usize {
-        (128 - p.x - p.y) as usize
+        (64 - p.x - p.y) as usize
     }
 }
 
@@ -37,184 +36,186 @@ fn neighbours(pt: &Pt) -> Vec<Pt> {
     if 0 < pt.x {
         ret.push(Pt::new(pt.x - 1, pt.y))
     }
-    if pt.x < 64 {
+    if pt.x < 32 {
         ret.push(Pt::new(pt.x + 1, pt.y))
     }
     if 0 < pt.y {
         ret.push(Pt::new(pt.x, pt.y - 1))
     }
-    if pt.y < 64 {
+    if pt.y < 32 {
         ret.push(Pt::new(pt.x, pt.y + 1))
     }
     ret
 }
 
-#[bench]
-fn corner_to_corner_astar(b: &mut Bencher) {
-    b.iter(|| {
-        assert_ne!(
-            astar(
-                &Pt::new(0, 0),
-                |n| neighbours(n).into_iter().map(|n| (n, 1)),
-                Pt::heuristic,
-                |n| n.x == 64 && n.y == 64,
-            ),
-            None
-        )
-    })
-}
-
-#[bench]
-fn corner_to_corner_bfs(b: &mut Bencher) {
-    b.iter(|| {
-        assert_ne!(
-            bfs(
-                &Pt::new(0, 0),
-                |n| neighbours(n),
-                |n| n.x == 64 && n.y == 64,
-            ),
-            None
-        )
-    })
-}
-
-#[bench]
-#[ignore] // Stack explosion, as expected
-fn corner_to_corner_dfs(b: &mut Bencher) {
-    b.iter(|| {
-        assert_ne!(
-            dfs(Pt::new(0, 0), |n| neighbours(n), |n| n.x == 64 && n.y == 64),
-            None
-        )
-    })
-}
-
-#[bench]
-fn corner_to_corner_iddfs(b: &mut Bencher) {
-    b.iter(|| {
-        assert_ne!(
-            iddfs(Pt::new(0, 0), |n| neighbours(n), |n| n.x == 5 && n.y == 5,),
-            None
-        )
-    })
-}
-
-#[bench]
-fn corner_to_corner_dijkstra(b: &mut Bencher) {
-    b.iter(|| {
-        assert_ne!(
-            dijkstra(
-                &Pt::new(0, 0),
-                |n| neighbours(n).into_iter().map(|n| (n, 1)),
-                |n| n.x == 64 && n.y == 64,
-            ),
-            None
-        )
+fn corner_to_corner_astar(c: &mut Criterion) {
+    c.bench_function("fill-corner_to_corner_astar", |b| {
+        b.iter(|| {
+            assert_ne!(
+                astar(
+                    &Pt::new(0, 0),
+                    |n| neighbours(n).into_iter().map(|n| (n, 1)),
+                    Pt::heuristic,
+                    |n| n.x == 32 && n.y == 32,
+                ),
+                None
+            )
+        })
     });
 }
 
-#[bench]
-fn corner_to_corner_fringe(b: &mut Bencher) {
-    b.iter(|| {
-        assert_ne!(
-            fringe(
-                &Pt::new(0, 0),
-                |n| neighbours(n).into_iter().map(|n| (n, 1)),
-                Pt::heuristic,
-                |n| n.x == 64 && n.y == 64,
-            ),
-            None
-        )
-    })
-}
-
-#[bench]
-fn corner_to_corner_idastar(b: &mut Bencher) {
-    b.iter(|| {
-        assert_ne!(
-            idastar(
-                &Pt::new(0, 0),
-                |n| neighbours(n).into_iter().map(|n| (n, 1)),
-                Pt::heuristic,
-                |n| n.x == 64 && n.y == 64,
-            ),
-            None
-        )
-    })
-}
-
-#[bench]
-fn no_path_astar(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(
-            astar(
-                &Pt::new(2, 3),
-                |n| neighbours(n).into_iter().map(|n| (n, 1)),
-                |_| 1,
-                |_| false,
-            ),
-            None
-        )
-    })
-}
-
-#[bench]
-fn no_path_bfs(b: &mut Bencher) {
-    b.iter(|| assert_eq!(bfs(&Pt::new(2, 3), |n| neighbours(n), |_| false), None));
-}
-
-#[bench]
-fn no_path_iddfs(b: &mut Bencher) {
-    fn less_neighbours(pt: &Pt) -> Vec<Pt> {
-        let mut ret = Vec::with_capacity(4);
-        if 0 < pt.x {
-            ret.push(Pt::new(pt.x - 1, pt.y))
-        }
-        if pt.x < 4 {
-            ret.push(Pt::new(pt.x + 1, pt.y))
-        }
-        if 0 < pt.y {
-            ret.push(Pt::new(pt.x, pt.y - 1))
-        }
-        if pt.y < 4 {
-            ret.push(Pt::new(pt.x, pt.y + 1))
-        }
-        ret
-    }
-
-    b.iter(|| {
-        assert_eq!(
-            iddfs(Pt::new(2, 3), |n| less_neighbours(n), |_| false),
-            None
-        )
+fn corner_to_corner_bfs(c: &mut Criterion) {
+    c.bench_function("fill-corner_to_corner_bfs", |b| {
+        b.iter(|| {
+            assert_ne!(
+                bfs(
+                    &Pt::new(0, 0),
+                    |n| neighbours(n),
+                    |n| n.x == 32 && n.y == 32,
+                ),
+                None
+            )
+        })
     });
 }
 
-#[bench]
-fn no_path_dijkstra(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(
-            dijkstra(
-                &Pt::new(2, 3),
-                |n| neighbours(n).into_iter().map(|n| (n, 1)),
-                |_| false,
-            ),
-            None
-        )
+fn corner_to_corner_dfs(c: &mut Criterion) {
+    c.bench_function("fill-corner_to_corner_dfs", |b| {
+        b.iter(|| {
+            assert_ne!(
+                dfs(Pt::new(0, 0), |n| neighbours(n), |n| n.x == 32 && n.y == 32),
+                None
+            )
+        })
     });
 }
 
-#[bench]
-fn no_path_fringe(b: &mut Bencher) {
-    b.iter(|| {
-        assert_eq!(
-            fringe(
-                &Pt::new(2, 3),
-                |n| neighbours(n).into_iter().map(|n| (n, 1)),
-                |_| 1,
-                |_| false,
-            ),
-            None
-        )
-    })
+fn corner_to_corner_dijkstra(c: &mut Criterion) {
+    c.bench_function("fill-corner_to_corner_dijkstra", |b| {
+        b.iter(|| {
+            assert_ne!(
+                dijkstra(
+                    &Pt::new(0, 0),
+                    |n| neighbours(n).into_iter().map(|n| (n, 1)),
+                    |n| n.x == 32 && n.y == 32,
+                ),
+                None
+            )
+        })
+    });
 }
+
+fn corner_to_corner_fringe(c: &mut Criterion) {
+    c.bench_function("fill-corner_to_corner_fringe", |b| {
+        b.iter(|| {
+            assert_ne!(
+                fringe(
+                    &Pt::new(0, 0),
+                    |n| neighbours(n).into_iter().map(|n| (n, 1)),
+                    Pt::heuristic,
+                    |n| n.x == 32 && n.y == 32,
+                ),
+                None
+            )
+        })
+    });
+}
+
+fn corner_to_corner_idastar(c: &mut Criterion) {
+    c.bench_function("fill-corner_to_corner_idastar", |b| {
+        b.iter(|| {
+            assert_ne!(
+                idastar(
+                    &Pt::new(0, 0),
+                    |n| neighbours(n).into_iter().map(|n| (n, 1)),
+                    Pt::heuristic,
+                    |n| n.x == 32 && n.y == 32,
+                ),
+                None
+            )
+        })
+    });
+}
+
+fn corner_to_corner_iddfs(c: &mut Criterion) {
+    c.bench_function("fill-corner_to_corner_iddfs", |b| {
+        b.iter(|| {
+            assert_ne!(
+                iddfs(Pt::new(0, 0), |n| neighbours(n), |n| n.x == 5 && n.y == 5,),
+                None
+            )
+        })
+    });
+}
+
+fn no_path_astar(c: &mut Criterion) {
+    c.bench_function("fill-no_path_astar", |b| {
+        b.iter(|| {
+            assert_eq!(
+                astar(
+                    &Pt::new(2, 3),
+                    |n| neighbours(n).into_iter().map(|n| (n, 1)),
+                    |_| 1,
+                    |_| false,
+                ),
+                None
+            )
+        })
+    });
+}
+
+fn no_path_bfs(c: &mut Criterion) {
+    c.bench_function("fill-no_path_bfs", |b| {
+        b.iter(|| assert_eq!(bfs(&Pt::new(2, 3), |n| neighbours(n), |_| false), None))
+    });
+}
+
+fn no_path_dijkstra(c: &mut Criterion) {
+    c.bench_function("fill-no_path_dijkstra", |b| {
+        b.iter(|| {
+            assert_eq!(
+                dijkstra(
+                    &Pt::new(2, 3),
+                    |n| neighbours(n).into_iter().map(|n| (n, 1)),
+                    |_| false,
+                ),
+                None
+            )
+        })
+    });
+}
+
+fn no_path_fringe(c: &mut Criterion) {
+    c.bench_function("fill-no_path_fringe", |b| {
+        b.iter(|| {
+            assert_eq!(
+                fringe(
+                    &Pt::new(2, 3),
+                    |n| neighbours(n).into_iter().map(|n| (n, 1)),
+                    |_| 1,
+                    |_| false,
+                ),
+                None
+            )
+        })
+    });
+}
+
+// We do not test IDA* and IDDFS with the no_path variant
+// as the successive increasing full explorations of the
+// maze will take too much time.
+criterion_group!(
+    benches,
+    corner_to_corner_astar,
+    corner_to_corner_bfs,
+    corner_to_corner_dfs,
+    corner_to_corner_dijkstra,
+    corner_to_corner_fringe,
+    corner_to_corner_idastar,
+    corner_to_corner_iddfs,
+    no_path_astar,
+    no_path_bfs,
+    no_path_dijkstra,
+    no_path_fringe,
+);
+criterion_main!(benches);
--- benches/movingai.rs
+++ benches/movingai.rs
@@ -1,43 +1,46 @@
-#![feature(test)]
-
 // Test with files from https://movingai.com/benchmarks/
 
+#[macro_use]
+extern crate criterion;
 extern crate movingai;
 extern crate noisy_float;
 extern crate pathfinding;
-extern crate test;
 
+use criterion::Criterion;
 use movingai::parser::{parse_map_file, parse_scen_file};
 use movingai::{Coords2D, Map2D};
 use noisy_float::prelude::*;
 use pathfinding::directed::astar::astar;
 use std::path::Path;
-use test::Bencher;
 
 fn distance(a: &Coords2D, b: &Coords2D) -> R64 {
     r64((a.0 as f64 - b.0 as f64).hypot(a.1 as f64 - b.1 as f64))
 }
 
-#[bench]
-fn arena(b: &mut Bencher) {
-    b.iter(|| {
-        let map = parse_map_file(Path::new("./benches/arena.map")).unwrap();
-        let scenes = parse_scen_file(Path::new("./benches/arena.map.scen")).unwrap();
-        for scene in scenes {
-            let start = scene.start_pos;
-            let goal = scene.goal_pos;
-            let result = astar(
-                &start,
-                |&node| {
-                    map.neighbors(node)
-                        .into_iter()
-                        .map(move |n| (n, distance(&node, &n)))
-                },
-                |&node| distance(&node, &goal),
-                |&node| node == goal,
-            )
-            .unwrap();
-            assert!(result.1 - r64(scene.optimal_length).abs() <= 1e-4);
-        }
-    })
+pub fn arena(c: &mut Criterion) {
+    c.bench_function("arena", |b| {
+        b.iter(|| {
+            let map = parse_map_file(Path::new("./benches/arena.map")).unwrap();
+            let scenes = parse_scen_file(Path::new("./benches/arena.map.scen")).unwrap();
+            for scene in scenes {
+                let start = scene.start_pos;
+                let goal = scene.goal_pos;
+                let result = astar(
+                    &start,
+                    |&node| {
+                        map.neighbors(node)
+                            .into_iter()
+                            .map(move |n| (n, distance(&node, &n)))
+                    },
+                    |&node| distance(&node, &goal),
+                    |&node| node == goal,
+                )
+                .unwrap();
+                assert!(result.1 - r64(scene.optimal_length).abs() <= 1e-4);
+            }
+        })
+    });
 }
+
+criterion_group!(benches, arena);
+criterion_main!(benches);
