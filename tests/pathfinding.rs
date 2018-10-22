c2765fba7979120333d2e6539e0572c855aad2a4
Samuel Tardieu <sam@rfc1149.net>
2018-10-22 20:28:13 +0200

    Reformatting

--- tests/pathfinding.rs
+++ tests/pathfinding.rs
@@ -168,11 +168,9 @@ mod ex2 {
         .unwrap();
         assert_eq!(cost, 8);
         assert_eq!(paths.len(), 1);
-        assert!(
-            paths
-                .iter()
-                .all(|path| path.iter().all(|&(nx, ny)| OPEN[ny][nx]))
-        );
+        assert!(paths
+            .iter()
+            .all(|path| path.iter().all(|&(nx, ny)| OPEN[ny][nx])));
         assert_eq!(counter, 15);
     }
 
@@ -192,11 +190,9 @@ mod ex2 {
         .unwrap();
         assert_eq!(cost, 9);
         assert_eq!(paths.len(), 3);
-        assert!(
-            paths
-                .iter()
-                .all(|path| path.iter().all(|&(nx, ny)| OPEN[ny][nx]))
-        );
+        assert!(paths
+            .iter()
+            .all(|path| path.iter().all(|&(nx, ny)| OPEN[ny][nx])));
         assert_eq!(counter, 18);
     }
 
