f14cbd23de2ad6ef3ade1b992328e8f7f4602c8f
Samuel Tardieu <sam@rfc1149.net>
2018-11-22 12:19:21 +0100

    Implement a more generic Dijkstra

--- src/directed/dijkstra.rs
+++ src/directed/dijkstra.rs
@@ -1,10 +1,15 @@
 //! Compute a shortest path using the [Dijkstra search
 //! algorithm](https://en.wikipedia.org/wiki/Dijkstra's_algorithm).
 
+use indexmap::map::Entry::{Occupied, Vacant};
+use indexmap::IndexMap;
 use num_traits::Zero;
+use std::cmp::Ordering;
+use std::collections::{BinaryHeap, HashMap};
 use std::hash::Hash;
+use std::usize;
 
-use super::astar::astar;
+use super::reverse_path;
 
 /// Compute a shortest path using the [Dijkstra search
 /// algorithm](https://en.wikipedia.org/wiki/Dijkstra's_algorithm).
@@ -72,6 +77,190 @@ where
     IN: IntoIterator<Item = (N, C)>,
     FS: FnMut(&N) -> bool,
 {
-    let zero = Zero::zero();
-    astar(start, successors, |_| zero, success)
+    let (parents, reached) = run_dijkstra(start, successors, success);
+    reached.map(|target| {
+        (
+            reverse_path(&parents, |&(p, _)| p, target),
+            parents.get_index(target).unwrap().1 .1,
+        )
+    })
+}
+
+/// Determine all reachable nodes from a starting point as well as the minimum cost to
+/// reach them and a possible optimal parent node
+/// using the [Dijkstra search algorithm](https://en.wikipedia.org/wiki/Dijkstra's_algorithm).
+///
+/// - `start` is the starting node.
+/// - `successors` returns a list of successors for a given node, along with the cost for moving
+/// from the node to the successor.
+///
+/// The result is a map where every reachable node (not including `start`) is associated with
+/// an optimal parent node and a cost.
+///
+/// The [build_path] function can be used to build a full path from the starting point to one
+/// of the reachable targets.
+pub fn dijkstra_all<N, C, FN, IN>(start: &N, successors: FN) -> HashMap<N, (N, C)>
+where
+    N: Eq + Hash + Clone,
+    C: Zero + Ord + Copy,
+    FN: FnMut(&N) -> IN,
+    IN: IntoIterator<Item = (N, C)>,
+{
+    dijkstra_partial(start, successors, |_| false).0
+}
+
+/// Determine some reachable nodes from a starting point as well as the minimum cost to
+/// reach them and a possible optimal parent node
+/// using the [Dijkstra search algorithm](https://en.wikipedia.org/wiki/Dijkstra's_algorithm).
+///
+/// - `start` is the starting node.
+/// - `successors` returns a list of successors for a given node, along with the cost for moving
+/// from the node to the successor.
+/// - `stop` is a function which is called every time a node is examined (including `start`).
+///   A `true` return value will stop the algorithm.
+///
+/// The result is a map where every node examined before the algorithm stopped (not including
+/// `start`) is associated with an optimal parent node and a cost, as well as the node which
+/// caused the algorithm to stop if any.
+///
+/// The [build_path] function can be used to build a full path from the starting point to one
+/// of the reachable targets.
+pub fn dijkstra_partial<N, C, FN, IN, FS>(
+    start: &N,
+    successors: FN,
+    stop: FS,
+) -> (HashMap<N, (N, C)>, Option<N>)
+where
+    N: Eq + Hash + Clone,
+    C: Zero + Ord + Copy,
+    FN: FnMut(&N) -> IN,
+    IN: IntoIterator<Item = (N, C)>,
+    FS: FnMut(&N) -> bool,
+{
+    let (parents, reached) = run_dijkstra(start, successors, stop);
+    (
+        parents
+            .iter()
+            .skip(1)
+            .map(|(n, (p, c))| (n.clone(), (parents.get_index(*p).unwrap().0.clone(), *c)))
+            .collect(),
+        reached.map(|i| parents.get_index(i).unwrap().0.clone()),
+    )
+}
+
+fn run_dijkstra<N, C, FN, IN, FS>(
+    start: &N,
+    mut successors: FN,
+    mut stop: FS,
+) -> (IndexMap<N, (usize, C)>, Option<usize>)
+where
+    N: Eq + Hash + Clone,
+    C: Zero + Ord + Copy,
+    FN: FnMut(&N) -> IN,
+    IN: IntoIterator<Item = (N, C)>,
+    FS: FnMut(&N) -> bool,
+{
+    let mut to_see = BinaryHeap::new();
+    to_see.push(SmallestHolder {
+        cost: Zero::zero(),
+        index: 0,
+    });
+    let mut parents: IndexMap<N, (usize, C)> = IndexMap::new();
+    parents.insert(start.clone(), (usize::MAX, Zero::zero()));
+    let mut target_reached = None;
+    while let Some(SmallestHolder { cost, index }) = to_see.pop() {
+        let successors = {
+            let (node, &(_, c)) = parents.get_index(index).unwrap();
+            if stop(node) {
+                target_reached = Some(index);
+                break;
+            }
+            // We may have inserted a node several time into the binary heap if we found
+            // a better way to access it. Ensure that we are currently dealing with the
+            // best path and discard the others.
+            if cost > c {
+                continue;
+            }
+            successors(node)
+        };
+        for (successor, move_cost) in successors {
+            let new_cost = cost + move_cost;
+            let n;
+            match parents.entry(successor) {
+                Vacant(e) => {
+                    n = e.index();
+                    e.insert((index, new_cost));
+                }
+                Occupied(mut e) => {
+                    if e.get().1 > new_cost {
+                        n = e.index();
+                        e.insert((index, new_cost));
+                    } else {
+                        continue;
+                    }
+                }
+            }
+
+            to_see.push(SmallestHolder {
+                cost: new_cost,
+                index: n,
+            });
+        }
+    }
+    (parents, target_reached)
+}
+
+/// Build a path leading to a target according to a parents map, which must
+/// contain no loop. This function can be used after [dijkstra_all] or
+/// [dijkstra_partial] to build a path from a starting point to a reachable target.
+///
+/// - `target` is reachable target.
+/// - `parents` is a map containing an optimal parent (and an associated
+///    cost which is ignored here) for every reachable node.
+///
+/// This function returns a vector with a path from the farthest parent up to
+/// `target`, including `target` itself.
+///
+/// # Panics
+///
+/// If the `parents` map contains a loop, this function will attempt to build
+/// a path of infinite length and panic when memory is exhausted.
+#[cfg_attr(feature = "cargo-clippy", allow(implicit_hasher))]
+pub fn build_path<N, C>(target: &N, parents: &HashMap<N, (N, C)>) -> Vec<N>
+where
+    N: Eq + Hash + Clone,
+{
+    let mut rev = vec![target.clone()];
+    let mut next = target.clone();
+    while let Some((parent, _)) = parents.get(&next) {
+        rev.push(parent.clone());
+        next = parent.clone();
+    }
+    rev.reverse();
+    rev
+}
+
+struct SmallestHolder<K> {
+    cost: K,
+    index: usize,
+}
+
+impl<K: PartialEq> PartialEq for SmallestHolder<K> {
+    fn eq(&self, other: &SmallestHolder<K>) -> bool {
+        self.cost == other.cost
+    }
+}
+
+impl<K: PartialEq> Eq for SmallestHolder<K> {}
+
+impl<K: Ord> PartialOrd for SmallestHolder<K> {
+    fn partial_cmp(&self, other: &SmallestHolder<K>) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl<K: Ord> Ord for SmallestHolder<K> {
+    fn cmp(&self, other: &SmallestHolder<K>) -> Ordering {
+        other.cost.cmp(&self.cost)
+    }
 }
---
+++ tests/dijkstra-all.rs
@@ -0,0 +1,124 @@
+extern crate pathfinding;
+extern crate rand;
+
+use pathfinding::prelude::*;
+use rand::{rngs, Rng};
+
+fn build_network(size: usize) -> Matrix<usize> {
+    let mut network = Matrix::new(size, size, 0);
+    let mut rng = rngs::OsRng::new().unwrap();
+    for a in 0..size {
+        for b in 0..size {
+            if rng.gen_ratio(2, 3) {
+                network[&(a, b)] = rng.gen::<u16>() as usize;
+            }
+        }
+    }
+    network
+}
+
+fn neighbours(network: Matrix<usize>) -> impl FnMut(&usize) -> Vec<(usize, usize)> {
+    move |&a| {
+        (0..network.rows)
+            .filter_map(|b| match network[&(a, b)] {
+                0 => None,
+                p => Some((b, p)),
+            })
+            .collect()
+    }
+}
+
+#[test]
+fn all_paths() {
+    const SIZE: usize = 30;
+    let network = build_network(SIZE);
+    for start in 0..SIZE {
+        let paths = dijkstra_all(&start, neighbours(network.clone()));
+        for target in 0..SIZE {
+            if let Some((path, cost)) =
+                dijkstra(&start, neighbours(network.clone()), |&n| n == target)
+            {
+                if start == target {
+                    assert!(
+                        !paths.contains_key(&target),
+                        "path {} -> {} is present in {:?}",
+                        start,
+                        target,
+                        network
+                    );
+                } else {
+                    assert!(
+                        paths.contains_key(&target),
+                        "path {} -> {} is not found in {:?}",
+                        start,
+                        target,
+                        network
+                    );
+                    assert_eq!(
+                        cost, paths[&target].1,
+                        "cost differ in path {} -> {} in {:?}",
+                        start, target, network
+                    );
+                    let other_path = build_path(&target, &paths);
+                    // There might be several paths, but we know that internally we use the
+                    // same algorithm so the comparaison holds.
+                    assert_eq!(
+                        path, other_path,
+                        "path {} -> {} differ in {:?}: {:?} vs {:?}",
+                        start, target, network, path, other_path
+                    );
+                }
+            } else {
+                assert!(
+                    !paths.contains_key(&target),
+                    "path {} -> {} is present in {:?}",
+                    start,
+                    target,
+                    network
+                );
+            }
+        }
+    }
+}
+
+#[test]
+fn partial_paths() {
+    const SIZE: usize = 100;
+    let network = build_network(SIZE);
+    for start in 0..SIZE {
+        let (paths, reached) = dijkstra_partial(&start, neighbours(network.clone()), |&n| {
+            start != 0 && n != 0 && n != start && n % start == 0
+        });
+        if let Some(target) = reached {
+            assert!(target % start == 0, "bad stop condition");
+            // We cannot compare other paths since there is no guarantee that the
+            // paths variable is up-to-date as the algorithm stopped prematurely.
+            let cost = paths[&target].1;
+            let (path, dcost) =
+                dijkstra(&start, neighbours(network.clone()), |&n| n == target).unwrap();
+            assert_eq!(
+                cost, dcost,
+                "costs {} -> {} differ in {:?}",
+                start, target, network
+            );
+            let other_path = build_path(&target, &paths);
+            // There might be several paths, but we know that internally we use the
+            // same algorithm so the comparaison holds.
+            assert_eq!(
+                path, other_path,
+                "path {} -> {} differ in {:?}: {:?} vs {:?}",
+                start, target, network, path, other_path
+            );
+        } else if start != 0 && start <= (SIZE - 1) / 2 {
+            for target in 1..(SIZE / start) {
+                assert!(
+                    dijkstra(&start, neighbours(network.clone()), |&n| n == target).is_none(),
+                    "path {} -> {} found in {:?}",
+                    start,
+                    target,
+                    network
+                );
+            }
+        }
+    }
+}
