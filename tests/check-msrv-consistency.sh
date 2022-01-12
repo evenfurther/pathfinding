07d9c59b317a5dbfbe824bb55b6c9907647b57fb
Samuel Tardieu <sam@rfc1149.net>
2022-01-12 10:46:58 +0100

    Add MSRV and check for consistency

---
+++ tests/check-msrv-consistency.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+#
+
+set -e
+
+MSRV=$(grep rust-version Cargo.toml | sed -e 's/"$//' -e 's/.*"//')
+for f in README.md .github/workflows/tests.yml .github/bors.toml; do
+  if ! grep $MSRV $f > /dev/null 2>&1; then
+    echo "MSRV $MSRV not found in $f"
+    exit 1
+  fi
+done
