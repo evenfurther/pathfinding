ff1abc1de422b96a7105fa2d9229873fc1f9380c
Samuel Tardieu <sam@rfc1149.net>
2022-08-11 10:48:13 +0200

    Optimize Yen's algorithm
    When potential shortest paths have the same minimal cost, they can
    all be inserted into the result ("A set") as the algorithm will not
    find better alternatives in subsequent rounds.

--- src/directed/yen.rs
+++ src/directed/yen.rs
@@ -126,8 +126,8 @@ where
     // A min-heap to store our lowest-cost route candidate
     let mut k_routes = BinaryHeap::new();
     for ki in 0..(k - 1) {
-        if routes.len() <= ki {
-            // We have no more routes to explore
+        if routes.len() <= ki || routes.len() == k {
+            // We have no more routes to explore, or we have found enough.
             break;
         }
         // Take the most recent route to explore new spurs.
@@ -173,7 +173,23 @@ where
             }
         }
         if let Some(k_route) = k_routes.pop() {
-            routes.push(k_route.0);
+            let route = k_route.0;
+            let cost = route.cost;
+            routes.push(route);
+            // If we have other potential best routes with the same cost, we can insert
+            // them in the found routes since we will not find a better alternative.
+            while routes.len() < k {
+                if let Some(k_route) = k_routes.peek() {
+                    if k_route.0.cost == cost {
+                        let k_route = k_routes.pop().unwrap();
+                        routes.push(k_route.0);
+                    } else {
+                        break; // Other routes have higher cost
+                    }
+                } else {
+                    break; // No other routes
+                }
+            }
         }
     }
 
