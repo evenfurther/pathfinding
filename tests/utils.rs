b9ded3f72dd6937ac1550176acacdcecadb23e8e
Samuel Tardieu <sam@rfc1149.net>
2022-11-28 19:02:37 +0100

    Add tests for in_direction and move_in_direction

---
+++ tests/utils.rs
@@ -0,0 +1,24 @@
+use pathfinding::utils::*;
+
+#[test]
+fn in_direction_start_oob() {
+    assert_eq!(move_in_direction((8, 8), (-1, -1), (8, 8)), None);
+    assert_eq!(in_direction((8, 8), (-1, -1), (8, 8)).next(), None);
+}
+
+#[test]
+fn in_direction_end_oob() {
+    assert_eq!(move_in_direction((0, 0), (-1, -1), (8, 8)), None);
+    assert_eq!(in_direction((0, 0), (-1, -1), (8, 8)).next(), None);
+    assert_eq!(move_in_direction((7, 0), (1, -1), (8, 8)), None);
+    assert_eq!(in_direction((0, 7), (-1, 1), (8, 8)).next(), None);
+}
+
+#[test]
+fn in_direction_valid() {
+    assert_eq!(move_in_direction((1, 1), (1, 3), (2, 4)), None);
+    assert_eq!(
+        in_direction((1, 1), (1, 3), (8, 8)).collect::<Vec<_>>(),
+        vec![(2, 4), (3, 7)]
+    );
+}
