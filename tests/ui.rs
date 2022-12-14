8125192a9de5060dc60bc48c3ac309b62f5ba2df
Samuel Tardieu <sam@rfc1149.net>
2022-12-14 18:59:50 +0100

    Do not try to match macro error messages on stable

--- tests/ui.rs
+++ tests/ui.rs
@@ -1,7 +1,10 @@
 #[test]
 fn ui() {
-    if std::env::var("TOOLCHAIN").as_deref() != Ok("nightly") {
-        let t = trybuild::TestCases::new();
-        t.compile_fail("tests/ui/*.rs");
+    match std::env::var("TOOLCHAIN").as_deref() {
+        Ok("stable") | Ok("1.65.0") => (),
+        _ => {
+            let t = trybuild::TestCases::new();
+            t.compile_fail("tests/ui/*.rs");
+        }
     }
 }
