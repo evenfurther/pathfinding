885b5aba6d8995b9dcd5ad7ea5995a9ddcdd2f36
Samuel Tardieu <sam@rfc1149.net>
2023-09-22 19:07:57 +0200

    Replace bors by merge queue

--- tests/check-msrv-consistency.sh
+++ tests/check-msrv-consistency.sh
@@ -4,7 +4,7 @@
 set -e
 
 MSRV=$(grep rust-version Cargo.toml | sed -e 's/"$//' -e 's/.*"//')
-for f in README.md .github/workflows/tests.yml .github/bors.toml; do
+for f in README.md .github/workflows/tests.yml; do
   if ! grep $MSRV $f > /dev/null 2>&1; then
     echo "MSRV $MSRV not found in $f"
     exit 1
