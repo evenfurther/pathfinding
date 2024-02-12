279305d526c63bf99c7b05b629333601474f7298
Samuel Tardieu <sam@rfc1149.net>
2024-02-12 08:55:31 +0100

    fix(README): inline documentation to fix inner links to modules

--- tests/check-msrv-consistency.sh
+++ tests/check-msrv-consistency.sh
@@ -4,7 +4,7 @@
 set -e
 
 MSRV=$(awk -F '"' '/^rust-version =/ {print $2}' < Cargo.toml)
-if ! grep $MSRV README.md > /dev/null 2>&1; then
+if ! grep "Rust $MSRV" src/lib.rs > /dev/null 2>&1; then
   echo "MSRV $MSRV not found in README.md"
   exit 1
 fi
