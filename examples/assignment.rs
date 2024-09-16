2fb3c22aa6ff7e59917f08e8bcbc1a4c617b2d3e
Saveliy Yusufov <sy2685@columbia.edu>
2024-09-16 17:01:11 -0400

    Add example for profiling

---
+++ examples/assignment.rs
@@ -0,0 +1,18 @@
+use pathfinding::bertsekas::{forward, Auction};
+use pathfinding::matrix::Matrix;
+use rand::Rng;
+
+fn generate_random_matrix(rows: usize, cols: usize) -> Matrix<f64> {
+    let mut rng = rand::thread_rng();
+    Matrix::from_fn(rows, cols, |_| rng.gen_range(0.0..100.0))
+}
+
+fn main() {
+    // let sizes = vec![100, 250, 500];
+
+    let size = 500;
+    let matrix = generate_random_matrix(size, size);
+    let mut auction_data = Auction::new(&matrix);
+
+    forward(&mut auction_data);
+}
