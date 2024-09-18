ea0ec6c5a6d3116cd9f9c9014629a650728e9b61
Saveliy Yusufov <sy2685@columbia.edu>
2024-09-17 20:16:34 -0400

    Fix example so both matrices are generated at once

--- examples/assignment.rs
+++ examples/assignment.rs
@@ -1,18 +1,49 @@
 use pathfinding::bertsekas::{bertsekas_aaap, Auction};
+use pathfinding::kuhn_munkres::kuhn_munkres;
 use pathfinding::matrix::Matrix;
 use rand::Rng;
+use std::time::Instant;
 
-fn generate_random_matrix(rows: usize, cols: usize) -> Matrix<f64> {
+fn generate_random_matrices(rows: usize, cols: usize) -> (Matrix<f64>, Matrix<i64>) {
     let mut rng = rand::thread_rng();
-    Matrix::from_fn(rows, cols, |_| rng.gen_range(0.0..100.0))
+    let random_numbers: Vec<i64> = (0..rows * cols).map(|_| rng.gen_range(0..100)).collect();
+
+    let matrix_int = Matrix::from_vec(rows, cols, random_numbers.clone()).unwrap();
+    let matrix_float = Matrix::from_vec(
+        rows,
+        cols,
+        random_numbers.into_iter().map(|x| x as f64).collect(),
+    )
+    .unwrap();
+
+    (matrix_float, matrix_int)
 }
 
 fn main() {
-    // let sizes = vec![100, 250, 500];
+    let sizes = vec![5, 10, 50, 100, 250, 500, 1000];
 
-    let size = 500;
-    let matrix = generate_random_matrix(size, size);
-    let mut auction_data = Auction::new(&matrix);
+    for &size in &sizes {
+        println!("Matrix size: {size} x {size}");
+        let (f_matrix, i_matrix) = generate_random_matrices(size, size);
 
-    bertsekas_aaap(&mut auction_data);
+        // Bertsekas solver
+        let start = Instant::now();
+        let mut auction_data = Auction::new(&f_matrix);
+        bertsekas_aaap(&mut auction_data);
+        let score = auction_data.score().unwrap();
+        let duration = start.elapsed();
+        println!(
+            "Bertsekas algo time elapsed: {:?} with score: {score}",
+            duration
+        );
+
+        // Kuhn-Munkres solver
+        let start = Instant::now();
+        let (score, _) = kuhn_munkres(&i_matrix);
+        let duration = start.elapsed();
+        println!(
+            "Hungarian algo time elapsed: {:?} with score: {score}",
+            duration
+        );
+    }
 }
