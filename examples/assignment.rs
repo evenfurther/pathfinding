d2479d93797d4d23067856d033b181676cd986a5
Saveliy Yusufov <sy2685@columbia.edu>
2024-09-18 16:20:40 -0400

    Fix example of assignment

--- examples/assignment.rs
+++ examples/assignment.rs
@@ -6,7 +6,7 @@ use std::time::Instant;
 
 fn generate_random_matrices(rows: usize, cols: usize) -> (Matrix<f64>, Matrix<i64>) {
     let mut rng = rand::thread_rng();
-    let random_numbers: Vec<i64> = (0..rows * cols).map(|_| rng.gen_range(0..100)).collect();
+    let random_numbers: Vec<i64> = (0..rows * cols).map(|_| rng.gen_range(1..500)).collect();
 
     let matrix_int = Matrix::from_vec(rows, cols, random_numbers.clone()).unwrap();
     let matrix_float = Matrix::from_vec(
@@ -20,13 +20,12 @@ fn generate_random_matrices(rows: usize, cols: usize) -> (Matrix<f64>, Matrix<i6
 }
 
 fn main() {
-    let sizes = vec![5, 10, 50, 100, 250, 500, 1000];
+    let sizes: [usize; 9] = [5, 10, 50, 100, 250, 500, 1000, 2000, 4000];
 
     for &size in &sizes {
         println!("Matrix size: {size} x {size}");
         let (f_matrix, i_matrix) = generate_random_matrices(size, size);
 
-        // Bertsekas solver
         let start = Instant::now();
         let mut auction_data = Auction::new(&f_matrix);
         bertsekas_aaap(&mut auction_data);
@@ -37,12 +36,11 @@ fn main() {
             duration
         );
 
-        // Kuhn-Munkres solver
         let start = Instant::now();
         let (score, _) = kuhn_munkres(&i_matrix);
         let duration = start.elapsed();
         println!(
-            "Hungarian algo time elapsed: {:?} with score: {score}",
+            "Hungarian algo time elapsed: {:?} with score: {score}\n",
             duration
         );
     }
