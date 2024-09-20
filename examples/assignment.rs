322f8a16eaea2f8d2813e34db4696eb2571f6849
Saveliy Yusufov <sy2685@columbia.edu>
2024-09-19 20:07:19 -0400

    Create simple csv output for example assignment

--- examples/assignment.rs
+++ examples/assignment.rs
@@ -20,28 +20,23 @@ fn generate_random_matrices(rows: usize, cols: usize) -> (Matrix<f64>, Matrix<i6
 }
 
 fn main() {
-    let sizes: [usize; 9] = [5, 10, 50, 100, 250, 500, 1000, 2000, 4000];
+    let sizes: [usize; 9] = [5, 10, 50, 100, 250, 500, 1000, 2500, 5000];
+
+    println!("Algorithm, Matrix Size, Time (ns), Score");
 
     for &size in &sizes {
-        println!("Matrix size: {size} x {size}");
         let (f_matrix, i_matrix) = generate_random_matrices(size, size);
 
-        let start = Instant::now();
+        let now = Instant::now();
         let mut auction_data = Auction::new(&f_matrix);
         bertsekas_aaap(&mut auction_data);
         let score = auction_data.score().unwrap();
-        let duration = start.elapsed();
-        println!(
-            "Bertsekas algo time elapsed: {:?} with score: {score}",
-            duration
-        );
+        let elapsed = now.elapsed().as_nanos();
+        println!("Bertsekas, {size}x{size}, {elapsed}, {score}");
 
-        let start = Instant::now();
+        let now = Instant::now();
         let (score, _) = kuhn_munkres(&i_matrix);
-        let duration = start.elapsed();
-        println!(
-            "Hungarian algo time elapsed: {:?} with score: {score}\n",
-            duration
-        );
+        let elapsed = now.elapsed().as_nanos();
+        println!("Kuhn_Munkres, {size}x{size}, {elapsed}, {score}");
     }
 }
