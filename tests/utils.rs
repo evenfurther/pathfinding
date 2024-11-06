86f05b6ade0a945d349dff76d3aaab92558bd031
Samuel Tardieu <sam@rfc1149.net>
2024-11-06 22:08:00 +0100

    fix(tests): new test for `utils` module

--- tests/utils.rs
+++ tests/utils.rs
@@ -15,6 +15,13 @@ fn in_direction_end_oob() {
 }
 
 #[test]
+fn in_direction_invalid() {
+    assert_eq!(in_direction((0, 8), (-1, 1), (8, 8)).next(), None);
+    assert_eq!(in_direction((8, 0), (-1, 1), (8, 8)).next(), None);
+    assert_eq!(in_direction((0, 7), (0, 0), (8, 8)).next(), None);
+}
+
+#[test]
 fn in_direction_valid() {
     assert_eq!(move_in_direction((1, 1), (1, 3), (2, 4)), None);
     assert_eq!(
