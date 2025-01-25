aa33d70578e293b4493e4d1465626c10b3c29a66
Samuel Tardieu <sam@rfc1149.net>
2025-01-25 12:52:07 +0100

    fix(CI): give the right file name in output message

--- tests/check-msrv-consistency.sh
+++ tests/check-msrv-consistency.sh
@@ -5,6 +5,6 @@ set -e
 
 MSRV=$(awk -F '"' '/^rust-version =/ {print $2}' < Cargo.toml)
 if ! grep "Rust $MSRV" src/lib.rs > /dev/null 2>&1; then
-  echo "MSRV $MSRV not found in README.md"
+  echo "MSRV $MSRV not found in src/lib.rs"
   exit 1
 fi
